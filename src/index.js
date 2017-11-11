bfdc373acbe842207b1a419c79b3d8791c00e6fc
Cody Wall <cody@cmwall.io>
2017-11-10 20:30:57 -0600

    Add back default settings
    I accidentally removed the default settings.
    
    Woops.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import React from "react"
 
+const defaultSettings = {
+  ssr: false,
+  delay: 0,
+  duration: 1,
+  delayOffset: 0.1,
+  initialStyle: { opacity: 0 },
+  animation: `{
+    from { opacity: 0; transform: translateY(-10px); }
+    to { opacity: 1; transform: translateY(0); }
+  }`,
+  animationName: "blanketAnimationFadeIn",
+}
+
 export default class BlanketAnimation extends React.Component {
   applyAnimationStyles(child, index) {
     const {
-      ssr,
-      animationName,
-      duration,
-      delay,
-      delayOffset,
-      initialStyle,
+      delay = defaultSettings.delay,
+      duration = defaultSettings.duration,
+      delayOffset = defaultSettings.delayOffset,
+      animationName = defaultSettings.animationName,
+      initialStyle = animation === defaultSettings.animation
+        ? defaultSettings.initialStyle
+        : {},
     } = this.props
 
     const style = Object.assign(
       {},
       {
         ...child.props.style,
-        animationName: ssr || animationName,
+        animationName: animationName,
         duration: `${duration}s`,
         delay: `${delay + index * delayOffset}s`,
         animationFillMode: "forwards",
@@ -30,11 +44,8 @@ export default class BlanketAnimation extends React.Component {
     const {
       children,
       ssr,
-      animation = `{
-        from { opacity: 0; transform: translateY(-10px); }
-        to { opacity: 1; transform: translateY(0); }
-      }`,
-      animationName = "blanketAnimationFadeIn",
+      animation = defaultSettings.animation,
+      animationName = defaultSettings.animationName,
     } = this.props
 
     if (!children) return ""
