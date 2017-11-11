e3a7580e5b5939c584df6f4dc354b2e434ce0db0
Cody Wall <cody@cmwall.io>
2017-11-10 20:54:56 -0600

    Version 1.1.0
    - Default props the correct way
    - Abstract function logic
    - Apply style to stylesheet only on contructor, not every render
    - Fix inline style CSS rule names

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
 import React from "react"
 
-const defaultSettings = {
-  ssr: false,
-  delay: 0,
-  duration: 1,
-  delayOffset: 0.1,
-  initialStyle: { opacity: 0 },
-  animation: `{
-    from { opacity: 0; transform: translateY(-10px); }
-    to { opacity: 1; transform: translateY(0); }
-  }`,
-  animationName: "blanketAnimationFadeIn",
-}
-
 export default class BlanketAnimation extends React.Component {
-  applyAnimationStyles(child, index) {
-    const {
-      delay = defaultSettings.delay,
-      duration = defaultSettings.duration,
-      delayOffset = defaultSettings.delayOffset,
-      animationName = defaultSettings.animationName,
-      initialStyle = this.props.animation === defaultSettings.animation
-        ? defaultSettings.initialStyle
-        : {},
-    } = this.props
+  static defaultProps = {
+    ssr: false,
+    delay: 0,
+    duration: 1,
+    delayOffset: 0.1,
+    initialStyle: { opacity: 0 },
+    animation: `{
+      from { opacity: 0; transform: translateY(-10px); }
+      to { opacity: 1; transform: translateY(0); }
+    }`,
+    animationName: "blanketAnimationFadeIn",
+  }
 
+  constructor(props) {
+    super(props)
+
+    const { ssr, animation, animationName } = this.props
+
+    if (!ssr && document && document.styleSheets[0]) {
+      const styleSheet = document.styleSheets[0]
+      const appliedAnimation = `@keyframes ${animationName} ${animation}`
+
+      styleSheet.insertRule(appliedAnimation, styleSheet.cssRules.length)
+    }
+  }
+
+  applyAnimationStyles(child, index) {
     const style = Object.assign(
       {},
       {
         ...child.props.style,
-        animationName: animationName,
-        duration: `${duration}s`,
-        delay: `${delay + index * delayOffset}s`,
+        animationName: this.props.animationName,
+        animationDuration: `${this.props.duration}s`,
+        animationDelay: `${this.props.delay + index * this.props.delayOffset}s`,
         animationFillMode: "forwards",
-        ...initialStyle,
+        ...this.props.initialStyle,
       }
     )
 
@@ -41,20 +44,9 @@ export default class BlanketAnimation extends React.Component {
   }
 
   render() {
-    const {
-      children,
-      ssr,
-      animation = defaultSettings.animation,
-      animationName = defaultSettings.animationName,
-    } = this.props
+    const { children } = this.props
 
     if (!children) return ""
-
-    if (!ssr && document && document.styleSheets[0]) {
-      const styleSheet = document.styleSheets[0]
-      const appliedAnimation = `@keyframes ${animationName} ${animation}`
-      styleSheet.insertRule(appliedAnimation, styleSheet.cssRules.length)
-    }
 
     return React.Children.map(children, (child, index) =>
       this.applyAnimationStyles(child, index)
